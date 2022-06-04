7f0302e454334c5f506e0a732272220227ce2ec6
Hussein <hussendon3@gmail.com>
2022-06-04 20:23:55 +0300

    testing1

--- components/index.js
+++ components/index.js
@@ -1,9 +1,10 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import Menuitem from './Menuitem/Menuitem';
+
 import Navbar from './Navbar/Navbar';
 import Model from "./Model/Model"
+import Menuitem from './Menuitem/Menuitem';
 
 export {
   SubHeading,
